f5cdb2c8727c91b4efa067f8f47bf7ad8f495e52
Jessica <jessica.giorgi@edu.unito.it>
2022-04-22 15:23:17 +0200

    commented

--- Esercizio1/modules/sort.c
+++ Esercizio1/modules/sort.c
@@ -4,6 +4,11 @@
 
 #define INITIAL_CAPACITY_QS 2
 
+/* 
+ * sort_create(): checks that parametres are not NULL and allocs memory 
+ * for the data structure sort.
+ * It then initialises the struct's fields. 
+ */
 Sort *sort_create(int (*qs_precedes)(void *, void *)){	
   if (qs_precedes == NULL){
 	  fprintf(stderr, "sort_create: precedes parameter cannot be NULL");
@@ -25,6 +30,10 @@ Sort *sort_create(int (*qs_precedes)(void *, void *)){
   return sort;
 }
 
+/*
+ * sort_size(): checks if the struct is NULL, if it's not it returns
+ * the size field.  
+ */
 unsigned long sort_size(Sort *sort){	
   if (sort == NULL){
     fprintf(stderr, "sort_size: sort parameter cannot be NULL");
@@ -33,6 +42,11 @@ unsigned long sort_size(Sort *sort){
   return sort->size;
 }
 
+/*
+ * sort_add(): checks if either the data structure or the element parametre is null,
+ * then reallocs memory to fit the new element. The capacity and size fields are updated 
+ * and the element is inserted in its place.  
+ */
 void sort_add(Sort *sort, void *element){	
   if (sort == NULL){
     fprintf(stderr, "sort_add: sort parameter cannot be NULL");
@@ -54,7 +68,9 @@ void sort_add(Sort *sort, void *element){
   (sort->size)++;
 }
 
-/* Returns the element at i index */
+/* 
+ * sort_get():returns the element at i index 
+ */
 void *sort_get(Sort *sort, unsigned long i){	
   if (sort == NULL){
     fprintf(stderr, "sort_get: unable to allocate the memory");
@@ -67,6 +83,9 @@ void *sort_get(Sort *sort, unsigned long i){
   return sort->array[i];
 }
 
+/*
+ * sort_free_memory(): frees memory after checking whether the structure is NULL or not.
+ */
 void *sort_free_memory(Sort *sort){	
   if (sort == NULL){
     fprintf(stderr, "sort_free_memory: unable to allocate the memory");
@@ -76,6 +95,9 @@ void *sort_free_memory(Sort *sort){
   free(sort);
 }
 
+/*
+ * swap(): swaps two elements. Needed by the quick_sort algorithm.
+ */
 void swap(Sort *sort, int p, int j){	
   void **tmp;
   tmp = sort->array[p];
@@ -85,20 +107,25 @@ void swap(Sort *sort, int p, int j){
 
 /*** Quicksort ***/
 /*
- * quick_sort: Check base condition, if parametres are correct partition is called and the pivot is returned. (?)
+ * quick_sort: Check base condition, if parametres are correct 
+ * partition is called and the result is stored. 
  * Then quick_sort is called on the subarrays.  
 */
 void quick_sort(Sort *sort, int low, int high){
-  // base condition
   if (low >= high){
     return;
   }
-  int pivot = partition(sort, low, high); 
-  quick_sort(sort, low, pivot);
-  quick_sort(sort, pivot + 1, high);
+  int tmp = partition(sort, low, high); 
+  quick_sort(sort, low, tmp);
+  quick_sort(sort, tmp + 1, high);
 }
 
-/* Partition using Hoare's Partitioning scheme */
+/* 
+ * Partition using Hoare's Partitioning scheme 
+ * This procedure uses two indices set at the beginning and at the end of the array.
+ * They move towords each other until there's an inversion to be made. 
+ * When the indices meet, the procedure stops and the last index is returned. 
+ */
 int partition(Sort *sort, int array_start, int array_end){	
   int i = array_start - 1;
   int j = array_end + 1;
@@ -117,10 +144,11 @@ int partition(Sort *sort, int array_start, int array_end){
 }
 
 /*** Binary Insertion Sort ***/
-/*  (????) 
+/*   
  * binary_search: mid is set to *parte intera inferiore*. 
- * If item is in mid position, mid + 1 is returned.
- * else if item is greater ...... 
+ * If item is in mid position, mid + 1 is returned,  else if item is
+ * greater it sets low to mid + 1, then returns it.
+ * It is returned the index where insertion_sort() will insert the element to order the data.  
  */
 int binary_search(Sort *sort, void *item, int low, int high){	
   while (low <= high) {
@@ -135,14 +163,15 @@ int binary_search(Sort *sort, void *item, int low, int high){
   return low;
 }
 
-/*  (????)
- * insertion_sort: finds the location where selected (item at i index) should be inseretd 
- * calling binary_search(), then moves all elements after location to create space
+/*  
+ * insertion_sort: selected (item at i index) stores the element to be inserted. 
+ * It then finds the location where selected should be inseretd by
+ * calling binary_search(), then moves all elements after location to create space. 
  */
-void insertion_sort(Sort *sort, int n){	
+void insertion_sort(Sort *sort, int length){	
   int i, loc, j, k;
   void *selected; 
-  for (i = 1; i <= n; ++i) {
+  for (i = 1; i <= length; i++) {
     j = i - 1;
     selected = sort->array[i];
     loc = binary_search(sort, selected, 0, j);
