9a9a403d9bbe45928b4fb5e955db826c98a2a055
Alessio Ghione <ghione.alessio@educ.di.unito.it>
2022-05-27 11:25:13 +0200

    converted adjlist froma arrayList to hashMap

--- Esercizio4/src/Graph.java
+++ Esercizio4/src/Graph.java
@@ -1,87 +1,72 @@
 package src;
 import java.util.HashMap;
-import java.util.Map;
+
 import java.util.ArrayList;
 
 public class Graph<T extends Comparable<T>, S> {
 
-  private HashMap<T, ArrayList<Vector<T, S>>> nodeMap;
+  private HashMap<T, HashMap<T, S>> nodeMap;
   private boolean directed_graph;
 
   public Graph(boolean direct) {
-    this.nodeMap = new HashMap<T, ArrayList<Vector<T,S>>>();
+    this.nodeMap = new HashMap<T, HashMap<T,S>>();
     this.directed_graph = direct;
   }
 
   public void addNode(T elem) {
-    nodeMap.put(elem, new ArrayList<Vector<T, S>>());
+    nodeMap.put(elem, new HashMap<T, S>());
   }
 
-  public void add_edge(T node1, T node2, S label) {
+  public void addEdge(T node1, T node2, S label) {
 
-    (nodeMap.get(node1)).add(new Vector<T, S>(node2, label));
+    (nodeMap.get(node1)).put(node2, label);
 
-    if (!is_directed()) {
-      nodeMap.get(node2).add(new Vector<T, S>(node1, label));
+    if (!isDirected()) {
+      nodeMap.get(node2).put(node1, label);
     }
 
   }
 
-  public boolean is_directed() {
+  public boolean isDirected() {
     return directed_graph;
   }
 
-  public boolean contains_node(T node) {
+  public boolean containsNode(T node) {
     if(nodeMap.containsKey(node))
         return true;
     return false; 
   }
 
-  public boolean contains_edge(T node1, T node2) {
-    ArrayList<Vector<T, S>> tmp = nodeMap.get(node1);
-    if(tmp == null)
-      return false;
-    for (int i = 0; i < tmp.size(); i++) {
-      if (tmp.get(i).getVertex().compareTo(node2) == 0)
-        return true;
-    }
+  public boolean containsEdge(T node1, T node2) {
+    if(nodeMap.get(node1).containsKey(node2) == true)
+      return true;
     return false;
   }
 
-  public void remove_node(T node){
-    nodeMap.remove(node); 
-    for (Map.Entry<T, ArrayList<Vector<T, S>>> listOfNode : nodeMap.entrySet()){
-       for(int i = 0; i < listOfNode.getValue().size(); i++){
-          if(node.compareTo(listOfNode.getValue().get(i).getVertex()) == 0)
-          listOfNode.getValue().remove(i);
-        }
+  public void removeNode(T node){
+    nodeMap.remove(node);
+    for (HashMap<T, S> adjNodes : nodeMap.values()){
+      adjNodes.remove(node);
     }
   }
 
-  public void remove_edge(T node1, T node2) {
-    for(int i = 0; i < nodeMap.get(node1).size(); i++){
-      if((nodeMap.get(node1)).get(i).getVertex().compareTo(node2) == 0)
-        nodeMap.get(node1).remove(i);
-    }
-
-    if (!is_directed()) {
-      for(int i = 0; i < nodeMap.get(node2).size(); i++){
-        if((nodeMap.get(node2)).get(i).getVertex().compareTo(node1) == 0)
-          nodeMap.get(node2).remove(i);  
-      }
+  public void removeEdge(T node1, T node2) {
+    nodeMap.get(node1).remove(node2);
+    if (!isDirected()) {
+      nodeMap.get(node2).remove(node1);
     }
   }
 
-  public int get_nodes_num() {
+  public int getNodesNum() {
     return nodeMap.size();
   }
 
-  public int get_edges_num() {
+  public int getEdgesNum() {
     int edges_num = 0;
-    for (Map.Entry<T, ArrayList<Vector<T, S>>> listOfNode : nodeMap.entrySet()){
-      edges_num += listOfNode.getValue().size();
+    for (HashMap<T, S> adjNodes : nodeMap.values()){
+      edges_num += adjNodes.size();
     }
-    if(is_directed()){
+    if(isDirected()){
       return edges_num;
     }
     else{
@@ -90,19 +75,17 @@ public class Graph<T extends Comparable<T>, S> {
   }
 
   public ArrayList<T> getNodes(){
-    ArrayList<T> listOfNodes = new ArrayList<T>();
-    for(Map.Entry<T, ArrayList<Vector<T, S>>> nodesTmp : nodeMap.entrySet()){
-      listOfNodes.add(nodesTmp.getKey());
+    ArrayList<T> listOfNodes = new ArrayList<>();
+    for(T nodesTmp : nodeMap.keySet()){
+      listOfNodes.add(nodesTmp);
     }
-    System.out.println("list of nodes sizze = " + listOfNodes.size());
     return listOfNodes;
   }
 
   public ArrayList<T> getAdjNodes(T node){
     ArrayList<T> adjList = new ArrayList<>();
-    ArrayList<Vector<T,S>> tmpList = nodeMap.get(node);
-    for(int i = 0; i < tmpList.size(); i++){
-      adjList.add(tmpList.get(i).getVertex());
+    for(T nodesTmp : nodeMap.get(node).keySet()){
+      adjList.add(nodesTmp);
     }
     return adjList;
   }
@@ -112,72 +95,9 @@ public class Graph<T extends Comparable<T>, S> {
   // }
 
 
-  public S getLabel(T Node1, T Node2) throws Exception{
-    ArrayList<Vector<T, S>> lisOfadj = nodeMap.get(Node1);
-    for(int i = 0; i < lisOfadj.size(); i++){
-      if(lisOfadj.get(i).getVertex() == Node2)
-        return lisOfadj.get(i).getLabel();
-    }
-    throw new Exception("edge not existent");
+  public S getLabel(T node1, T node2) throws Exception{
+    return this.nodeMap.get(node1).get(node2);
   }
-
-  public S Dijkstra(T startNode, T destinationNode) throws Exception{
-    if(!is_directed())
-      throw new Exception();
-
-    
-  }
-
-  /*
-  pi_greco = padre
-  v = nodo generico
-  s = nodo di partenza
-  d = profondità,  
-  u = nodo boh, non agg capit
-  W(u,v) = peso arco tra u e v  
-  se la somma della profondità di u + il peso tra u e v è minore della profondità di v, aggiorno v(porto su kinda), nuoa profondità la somma(u.d + W(u,v)) e come padre u
-  */
-
-  // void dijkstra(int graph[V][V], int src)
-  // {
-  //     int dist[V]; // The output array.  dist[i] will hold the shortest
-  //     // distance from src to i
-  //     bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-  //     // path tree or shortest distance from src to i is finalized
-  //     // Initialize all distances as INFINITE and stpSet[] as false
-  //     for (int i = 0; i < V; i++)
-  //         dist[i] = INT_MAX, sptSet[i] = false;
-  //     // Distance of source vertex from itself is always 0
-  //     dist[src] = 0;
-  //     // Find shortest path for all vertices
-  //     for (int count = 0; count < V - 1; count++) {
-  //         // Pick the minimum distance vertex from the set of vertices not
-  //         // yet processed. u is always equal to src in the first iteration.
-  //         int u = minDistance(dist, sptSet);
-  //         // Mark the picked vertex as processed
-  //         sptSet[u] = true;
-  //         // Update dist value of the adjacent vertices of the picked vertex.
-  //         for (int v = 0; v < V; v++)
-  //             // Update dist[v] only if is not in sptSet, there is an edge from
-  //             // u to v, and total weight of path from src to  v through u is
-  //             // smaller than current value of dist[v]
-  //             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-  //                 && dist[u] + graph[u][v] < dist[v])
-  //                 dist[v] = dist[u] + graph[u][v];
-  //     }
-  // }
-
-//   Dijkstra(G, s)
-// Q ← V
-// for ∀v ∈ V do v .d← ∞, v .π ← nil
-// s.d← 0
-// s.π← nil
-// while Q 6 = ∅ do
-// u ← togli nodo con d minimo da Q
-// for ∀v ∈ adj[u] do
-// if v ∈ Q e u.d + W (u, v ) < v .d then
-// v .d ← u.d + W (u, v )
-// v .π ← u
 
 }
 
